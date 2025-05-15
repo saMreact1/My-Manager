401c446910430f89f1ec0ab5585419f9ea3dcdfd
sam <samueladeleke302@gmail.com>
2025-05-15 16:16:05 +0100

    removed the hardcoded port for the api and made it dynamic

--- api/server.js
+++ api/server.js
@@ -46,8 +46,8 @@ app.use('/tasks', taskRoutes); // Use task routes
 app.use('/admin', userRoutes);
 
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
-    console.log('✅ Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
 })
