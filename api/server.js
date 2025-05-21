e1fe32267349fbe4915d54012b2131d67a8ebab2
sam <samueladeleke302@gmail.com>
2025-05-21 10:11:51 +0100

    Responsiveness

--- api/server.js
+++ api/server.js
@@ -46,9 +46,9 @@ app.use('/admin', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log(`✅ Server is running on port`);
-})
-// app.listen(PORT, () => {
-//     console.log(`✅ Server is running on port ${PORT}`);
+// app.listen(3000, () => {
+//   console.log(`✅ Server is running on port`);
 // })
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
+})
