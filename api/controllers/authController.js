869161f0318fa8b1d8298141bed15c4233bf0511
Sam <samueladeleke392@gmail.com>
2025-06-23 20:18:01 +0100

    Added forgot and reset password functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -17,7 +17,7 @@ const createToken = (user) => {
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER, // Your email address
+    user: process.env.EMAIL_USERNAME, // Your email address
     pass: process.env.EMAIL_PASSWORD  // Your email password or app password
   }
 });
